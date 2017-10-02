2401a058deea3af6bde3a8e8e1cbc17546635645
Austin <elektricwebdesign@gmail.com>
2017-10-01 21:25:10 -0600

    took out the merge shit in dev config for webpack

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -2,7 +2,7 @@
     ./webpack.config.js
 */
 const webpack = require('webpack');
-const merge = require('webpack-merge');
+// const merge = require('webpack-merge');
 const path = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
